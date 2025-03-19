ac51d196dc4381c8c8d63536e14611efc4ee0f1f
AndroStav <www.stavinoga@gmail.com>
2025-03-19 14:38:19 +0200

    Зробив певні покращення в боті
    Додав блокування, можливість вказати канали по id, зробив логування в файл.

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 from telethon import TelegramClient, events
 from datetime import datetime, timezone
-import logging, re, configparser
+import logging, re, configparser, asyncio
 
+# логування
 logging.basicConfig(format='[%(levelname)s %(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
+                    level=logging.INFO, filename="bot.log")
 
+# завантаження конфігу
 def load_config(filename):
     config = configparser.ConfigParser()
     with open(filename, "r", encoding="utf-8") as f:
@@ -20,23 +22,32 @@ def load_config(filename):
 config = load_config("config.ini")
 if config is None:
     raise Exception("Конфігурація не завантажена")
-    
+
+# запис конфігів в змінні
 session = config["General"]["session"]
 api_id = config["General"]["api_id"]
 api_hash = config["General"]["api_hash"]
-    
+
 delay_long = int(config["Delays"]["delay_long"])
 delay_short = int(config["Delays"]["delay_short"])
-    
+
 destination_channel = config["Channels"]["destination_channel"]
 channels = config["Channels"]["channels"].split(", ")
-    
+
+# перевірка каналів, id чи ніки
+if not destination_channel.startswith('@'): destination_channel = int(destination_channel)
+channels = [channel if channel.startswith('@') else int(channel) for channel in channels]
+
 triggers = config["Triggers"]["triggers"].split(", ")
 black_list = config["Triggers"]["black_list"].split(", ")
 client = TelegramClient(session, api_id, api_hash)
 
+# блокування асинхронних функцій
+lock = asyncio.Lock()
+
 async def main():
     await client.start()
+    logging.info("Бота запущено")
     print("Бота запущено")
     
     me = await client.get_me()
@@ -46,6 +57,7 @@ async def main():
     print(f"Номер телефону: {me.phone}")
     print("-----------------------------------------")
     
+    # інфа про останнє повідомлення
     last_message = { "time": None, "found_triggers": set(), "number": 0}
     info_message = None
     
@@ -53,33 +65,40 @@ async def main():
     async def forwardmess(event):
         nonlocal last_message, info_message
         
-        if event.message.text:
-            found_triggers = {trigger for trigger in triggers if re.search(trigger, event.raw_text, re.IGNORECASE)}
-            
-            if found_triggers:
-                if not any(re.search(black_word, event.raw_text, re.IGNORECASE) for black_word in black_list):
-                    if not last_message["time"] or (event.date - last_message["time"]).seconds > delay_long \
-                        or (not any(re.search(trigger, event.raw_text, re.IGNORECASE) for trigger in last_message["found_triggers"]) and (event.date - last_message["time"]).seconds > delay_short):
+        async with lock:
+            if event.message.text:
+                found_triggers = {trigger for trigger in triggers if re.search(trigger, event.raw_text, re.IGNORECASE)}
+                
+                if found_triggers:
+                    if not any(re.search(black_word, event.raw_text, re.IGNORECASE) for black_word in black_list):
+                        time_diff = (event.date - last_message["time"]).seconds if last_message["time"] else 0
                         
-                        last_message["number"] = 1
-                        print(f"\n{event.date}")
-                        print(f"Пересилаю повідомлення:\n{event.raw_text}")
-                        await event.forward_to(destination_channel)
-                        
-                        last_message["time"] = event.date
-                        last_message["found_triggers"] = found_triggers
-                        info_message = None
-                    else:
-                        last_message["number"] += 1
-                        last_message["found_triggers"].update(found_triggers)
-
-                        trigger_text = ", ".join(last_message["found_triggers"])
-                        
-                        if not info_message:
-                            info_message = await client.send_message(destination_channel, f"Повідомлень: {last_message['number']}\nТригери:\n{trigger_text}")
+                        if not last_message["time"] \
+                        or time_diff > delay_long \
+                        or (not any(re.search(trigger, event.raw_text, re.IGNORECASE) for trigger in last_message["found_triggers"]) \
+                        and time_diff > delay_short):
+                            
+                            last_message["number"] = 1
+                            logging.info(f"Пересилаю повідомлення:\n{event.raw_text}")
+                            await event.forward_to(destination_channel)
+                            
+                            last_message["time"] = event.date
+                            last_message["found_triggers"] = found_triggers
+                            info_message = None
                         else:
-                            await info_message.edit(f"Повідомлень: {last_message['number']}\nТригери:\n{trigger_text}")
-                        print(f"\nПовідомлень: {last_message['number']}\nТригери:\n{trigger_text}")
+                            last_message["number"] += 1
+                            last_message["found_triggers"].update(found_triggers)
+
+                            trigger_text = ", ".join(last_message["found_triggers"])
+                            
+                            mess = f"Повідомлень: {last_message['number']}\nТригери:\n{trigger_text}"
+                            if not info_message:
+                                logging.info("Створюю інформаційне повідомлення")
+                                info_message = await client.send_message(destination_channel, mess)
+                            else:
+                                logging.info("Редагую інформаційне повідомлення")
+                                await info_message.edit(mess)
+                            logging.info(mess)
                         
     await client.run_until_disconnected()
 
